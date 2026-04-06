ccaf15182f6c60ca88f9ea2d6747092b68f876c4
mailinh2101 <2007210936@hufi.edu.vn>
2026-04-06 17:01:22 +0700

    refactor: Remove unused jQuery and Perfect Scrollbar scripts from error layout

--- resources/views/errors/layout.blade.php
+++ resources/views/errors/layout.blade.php
@@ -31,8 +31,6 @@
         </div>
     </div>
 
-    <script src="{{ asset('assets/vendor/libs/jquery/jquery.js') }}"></script>
-    <script src="{{ asset('assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.js') }}"></script>
     <script src="{{ asset('assets/js/bootstrap.js') }}"></script>
     <script src="{{ asset('assets/js/conca-sidebar.js') }}"></script>
     <script src="{{ asset('assets/js/conca.js') }}"></script>
